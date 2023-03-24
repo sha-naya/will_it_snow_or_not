ea0e64309b1b286992b7fab9162331507faf1858
Ayan Ashkenov <ayan.ashkenov@gmail.com>
2023-03-24 16:30:09 -0400

    added more preprocessing steps

--- data_exploration.R
+++ data_exploration.R
@@ -7,14 +7,6 @@ ncol(weather_data)
 # These columns can be used for classification:
 # AWND, PRCP, PRCP_ATTRIBUTES, SNOW_ATTRIBUTES, TAVG, TMAX, TMAX_ATTRIBUTES == TMIN_ATTRIBUTES,
 # TMIN, WDF2, WDF5, WSF2, WSF5, WT01, WT02, WT03, WT04, WT05, WT06, WT08, WT09, DATE ???
-# 
-# We can potentially use the DATE column by breaking it down into: SEASON column, MONTH column.
-#
-# Can we take the same type of data from a different location? That way we can use location as a
-# column.
-# Yes, that is possible. There are several stations in Boston area.
-# We can combine data from all of them for the past 5 years or so.
-# This way we get new columns: LOCATION/ELEVATION, YEAR, MONTH
 
 library(dplyr)
 dplyr::count(weather_data, weather_condition, sort = TRUE)
